27e0db8039dad1d0cab4a57d37bcd84cd7f12c6b
Marlon0144 <marlon.giraldo2@udea.edu.co>
2026-05-03 18:16:02 -0500

    Login valido para admin

--- src/main/java/com/codefactory/appstripe/security/infrastructure/config/DataSeeder.java
+++ src/main/java/com/codefactory/appstripe/security/infrastructure/config/DataSeeder.java
@@ -1,5 +1,6 @@
 package com.codefactory.appstripe.security.infrastructure.config;
 
+import com.codefactory.appstripe.security.application.port.IPasswordEncoderPort;
 import com.codefactory.appstripe.security.application.port.IUserRepositoryPort;
 import com.codefactory.appstripe.security.domain.User;
 import org.springframework.boot.CommandLineRunner;
@@ -10,13 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class DataSeeder {
 
     @Bean
-    public CommandLineRunner initData(IUserRepositoryPort userRepository) {
+    public CommandLineRunner initData(IUserRepositoryPort userRepository, IPasswordEncoderPort passwordEncoder) {
         return args -> {
             String adminEmail = "admin@paycore.com";
             if (!userRepository.existsByEmail(adminEmail)) {
                 User admin = User.builder()
                         .email(adminEmail)
-                        .password("$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HCGKK.Y3FzI/P6sL6KkEu") // admin123
+                        .password(passwordEncoder.encode("admin123"))
                         .role("ADMIN")
                         .accountActivated(true)
                         .twoFactorEnabled(false)
